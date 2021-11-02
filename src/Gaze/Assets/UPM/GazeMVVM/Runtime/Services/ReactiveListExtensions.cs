a0cccd6394602545b0bf713388116094a8420d5d
Renan Dresch Martins <renandreschmartins@gmail.com>
2021-11-02 18:22:07 -0300

    Add reactive list and stack extensions

---
+++ src/Gaze/Assets/UPM/GazeMVVM/Runtime/Services/ReactiveListExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Gaze.MVVM.Services
+{
+    public static class ReactiveListExtensions
+    {
+        public static void Add<T>(this ReactiveList<T> reactiveList, T newItem)
+        {
+            reactiveList.Writer.Add(newItem);
+        }
+        
+        public static bool Remove<T>(this ReactiveList<T> reactiveList, T item)
+        {
+            return reactiveList.Writer.Remove(item);
+        }
+        
+        public static void Insert<T>(this ReactiveList<T> reactiveList, int index, T newItem)
+        {
+            reactiveList.Writer.Insert(index, newItem);
+        }
+        
+        public static void RemoveAt<T>(this ReactiveList<T> reactiveList, int index)
+        {
+            reactiveList.Writer.RemoveAt(index);
+        }
+        
+        public static void Clear<T>(this ReactiveList<T> reactiveList)
+        {
+            reactiveList.Writer.Clear();
+        }
+    }
+}
---
+++ src/Gaze/Assets/UPM/GazeMVVM/Runtime/Services/ReactiveStackExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Gaze.MVVM.Services
+{
+    public static class ReactiveStackExtensions
+    {
+        public static void Push<T>(this ReactiveStack<T> reactiveStack, T newItem)
+        {
+            reactiveStack.Writer.Push(newItem);
+        }
+        
+        public static T Pop<T>(this ReactiveStack<T> reactiveStack)
+        {
+            return reactiveStack.Writer.Pop();
+        }
+
+        public static void Clear<T>(this ReactiveStack<T> reactiveStack)
+        {
+            reactiveStack.Writer.Clear();
+        }
+    }
+}
