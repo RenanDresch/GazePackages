c54cacf15268a1a625bb388e863bab1f6ff4359d
Renan Dresch <renandreschmartins@gmail.com>
2022-11-18 09:39:37 -0300

    Testing new reactive dictionary iteration method based on caching to avoid garbage

--- src/Gaze/Assets/UPM/GazeMCS/Runtime/ReactiveProperties/Concretes/ReactiveDictionary.cs
+++ src/Gaze/Assets/UPM/GazeMCS/Runtime/ReactiveProperties/Concretes/ReactiveDictionary.cs
@@ -13,18 +13,49 @@ namespace Gaze.MCS
         readonly Dictionary<TK, IReactiveProperty<TV>> internalDictionary;
         readonly Dictionary<TK, IReactiveProperty<TV>> initialCollection;
 
-        public ReactiveDictionary(int capacity) =>
+        bool cachingKeys;
+        int initialCapacity;
+        List<TK> keysCache; 
+
+        public ReactiveDictionary(int capacity)
+        {
+            initialCapacity = capacity;
             internalDictionary = new Dictionary<TK, IReactiveProperty<TV>>(capacity);
-        
-        public ReactiveDictionary(Dictionary<TK, IReactiveProperty<TV>> collection) => initialCollection = internalDictionary = collection;
+        }
+
+
+        public ReactiveDictionary(Dictionary<TK, IReactiveProperty<TV>> collection)
+        {
+            initialCapacity = collection.Count;
+            initialCollection = internalDictionary = collection;
+        }
+
+        public IReactiveDictionary<TK, TV> WithKeyCaching()
+        {
+            cachingKeys = true;
+            keysCache = new List<TK>(internalDictionary.Count > initialCapacity ? internalDictionary.Count : initialCapacity);
+            foreach (var key in internalDictionary.Keys)
+            {
+                keysCache.Add(key);
+            }
+
+            return this;
+        }
         
         protected override IReactiveDictionary<TK, TV> Builder => this;
         
         public override bool TryGetValue(TK index, out IReactiveProperty<TV> value) =>
             internalDictionary.TryGetValue(index, out value);
         protected override IReactiveProperty<TV> IndexGetter(TK index) => internalDictionary[index];
-        
-        protected override void IndexSetter(TK index, IReactiveProperty<TV> value) => internalDictionary[index] = value;
+
+        protected override void IndexSetter(TK index, IReactiveProperty<TV> value)
+        {
+            if (cachingKeys && !internalDictionary.ContainsKey(index))
+            {
+                keysCache.Add(index);
+            }
+            internalDictionary[index] = value;
+        }
 
         public override int Count => internalDictionary.Count;
 
@@ -33,6 +64,12 @@ namespace Gaze.MCS
         protected override (TK key, IReactiveProperty<TV> value) AddToCollection((TK key, TV value) item)
         {
             var reactiveValue = new ReactiveProperty<TV>(item.value);
+
+            if (cachingKeys)
+            {
+                keysCache.Add(item.key);
+            }
+           
             internalDictionary.Add(item.key, reactiveValue);
             OnCreateIndex(item.key, reactiveValue);
             return (item.key, reactiveValue);
@@ -41,6 +78,13 @@ namespace Gaze.MCS
         protected override (bool, TK key, IReactiveProperty<TV> value) RemoveFromCollection(TK key)
         {
             var success = TryGetValue(key, out var value);
+
+            if (cachingKeys)
+            {
+                keysCache.Remove(key);    
+            }
+            
+            internalDictionary.Remove(key);
             return (success, key, value);
         }
 
@@ -61,15 +105,18 @@ namespace Gaze.MCS
 
         public bool Contains(KeyValuePair<TK, IReactiveProperty<TV>> item) => internalDictionary.Contains(item);
         
-        public ICollection<TK> Keys => internalDictionary.Keys;
+        public List<TK> Keys => keysCache;
         
         public ICollection<IReactiveProperty<TV>> Values => internalDictionary.Values;
-        public IEnumerator<KeyValuePair<TK, IReactiveProperty<TV>>> GetEnumerator() => internalDictionary.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         public void Reset()
         {
             internalDictionary.Clear();
+
+            if (cachingKeys)
+            {
+                keysCache.Clear();
+            }
+            
             if (initialCollection != null)
             {
                 foreach (var (key, value) in initialCollection)
--- src/Gaze/Assets/UPM/GazeMCS/Runtime/ReactiveProperties/Interfaces/Collections/IReactiveDictionary.cs
+++ src/Gaze/Assets/UPM/GazeMCS/Runtime/ReactiveProperties/Interfaces/Collections/IReactiveDictionary.cs
@@ -5,9 +5,10 @@ namespace Gaze.MCS
     public interface IReactiveDictionary<TK, TV> :
         ICustomComparable<IReactiveDictionary<TK, TV>, TV>,
         IReactiveCollection<IReactiveDictionary<TK, TV>, TK, TV, (TK key, TV value), TK>,
-        IEnumerable<KeyValuePair<TK,IReactiveProperty<TV>>>,
         IReleasable, IResettable
     {
-        public void Add(TK key, TV value);
+        List<TK> Keys { get; }
+        IReactiveDictionary<TK, TV> WithKeyCaching();
+        void Add(TK key, TV value);
     }
 }
